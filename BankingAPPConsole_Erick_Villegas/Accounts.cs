0e515cead3d894b61dcd47d176a7c286c833b174
Erick Villegas <erick306@revature.net>
2022-03-22 18:41:36 -0700

    up to case 3 commit

--- BankingAPPConsole_Erick_Villegas/Accounts.cs
+++ BankingAPPConsole_Erick_Villegas/Accounts.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace BankingAPPConsole_Erick_Villegas
 {
@@ -13,66 +15,137 @@ namespace BankingAPPConsole_Erick_Villegas
         string v_accEmail;
         #endregion
             #region Account Properties
-        public int AccountNumber
+        public int p_accNo
         { 
             get{ return v_accNo;}
             set{v_accNo = value;}
         }
-
-        public string AccountName 
+        
+        public string p_accName 
         { 
             get{return v_accName;} 
             set{v_accName = value;} 
         }
 
-        public string AccountType 
+        public string p_accType 
         { 
             get{return v_accType;} 
             set{v_accType = value;}
         }
 
-        public double AccountBalance 
+        public double p_accBalance 
         { 
             get{return v_accBalance;} 
             set{v_accBalance = value;}
         }
 
-        public bool isAccountActive 
+        public bool p_isAccountActive 
         { 
             get{return v_isAccountActive;} 
             set{v_isAccountActive = value;}
         }
 
-        public string AccountEmail 
+        public string p_accEmail 
         { 
             get{return v_accEmail;} 
             set{v_accEmail = value;}
         }
         #endregion
             #region Methods
-        public double Withdraw(int withdraw_qty)
+        SqlConnection con = new SqlConnection(@"server=DESKTOP-9UJOUBT\Erick Villegas;database=BankingAPPConsole;integrated security=true");
+        public string AccountChanges(Accounts accChanges)
         {
-            AccountBalance = AccountBalance - withdraw_qty;
-            return AccountBalance;
+            SqlCommand cmd_changeAccount = new SqlCommand("insert into APP_Variables values(@p_accNo,@p_accName,@p_accType,@p_accBalalnce,@p_isAccountActive,@p_accEmail",con);
+            cmd_changeAccount.Parameters.AddWithValue("@p_accNo",accChanges.p_accNo);
+            cmd_changeAccount.Parameters.AddWithValue("@p_accName",accChanges.p_accName);
+            cmd_changeAccount.Parameters.AddWithValue("@p_accType",accChanges.p_accType);
+            cmd_changeAccount.Parameters.AddWithValue("@p_accBalance",accChanges.p_accBalance);
+            cmd_changeAccount.Parameters.AddWithValue("@p_isAccountActive",accChanges.p_isAccountActive);
+            cmd_changeAccount.Parameters.AddWithValue("@p_accEmail",accChanges.p_accEmail);
+
+            try
+            {
+                con.Open();
+                cmd_changeAccount.ExecuteNonQuery();
+            }
+            catch(SqlException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return "Changes made successfully";
+        }
+        public Accounts CheckingsBalance(int ChBalance)
+        {
+            Accounts Check = new Accounts();
+            SqlCommand cmd_BalanceCheck = new SqlCommand("select p_accBalance from APP_Variables where p_accType = @p_accType",con);
+            cmd_BalanceCheck.Parameters.AddWithValue("@p_accType",ChBalance);
+            SqlDataReader _read = null;
+            try
+            {
+                con.Open();
+                _read = cmd_BalanceCheck.ExecuteReader();
+                if (_read.Read()){
+                    Check.p_accType = Convert.ToString(ChBalance);
+
+                    return Check;
+                }
+            }
+            catch(SqlException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return Check;
+        }
+        public Accounts Withdraw(int withdraw_qty)
+        {
+            Accounts CkeckWithdraw = new Accounts();
+            p_accBalance = p_accBalance - withdraw_qty;
+            SqlCommand cmd_updateBalance = new SqlCommand("update App_Variables set p_accBalance = @newp_accBalance where p_accType = @p_accType",con);
+            cmd_updateBalance.Parameters.AddWithValue("@newp_accBalance",p_accBalance);
+             try
+            {
+                con.Open();
+                cmd_updateBalance.ExecuteNonQuery();
+            }
+            catch(SqlException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return "Withdraw Complete";
+            //p_accBalance = p_accBalance - withdraw_qty;
+            //return p_accBalance;
         }   
         public double Deposit(int deposit_qty)
         {
-            AccountBalance = AccountBalance + deposit_qty;
-            return AccountBalance;
+            p_accBalance = p_accBalance + deposit_qty;
+            return p_accBalance;
         }
         public void AccountDetails()
         {
-            Console.WriteLine("Account No: " + AccountNumber);
-            Console.WriteLine("Name: " + AccountName);
-            Console.WriteLine("Type: " + AccountType);
-            Console.WriteLine("Balance: " + AccountBalance);
-            Console.WriteLine("Active: " + isAccountActive);
-            Console.WriteLine("Email: " + AccountEmail);
+            Console.WriteLine("Account No: " + p_accNo);
+            Console.WriteLine("Name: " + p_accName);
+            Console.WriteLine("Type: " + p_accType);
+            Console.WriteLine("Balance: " + p_accBalance);
+            Console.WriteLine("Active: " + p_isAccountActive);
+            Console.WriteLine("Email: " + p_accEmail);
         }
-        public void CheckBalance()
-        {
-            Console.WriteLine("Balance: "+ AccountBalance);
-        }
+        
+        /*public string AdjustBalance(Accounts accChanges);
+            SqlCommand cmd_AdjBalance = new SqlCommand("update Account Balance set p_accBalance = @newP_accBalance, where p_accNo = @p_accNo",con);
+            cmd_AdjBalance.AddWithValue("@newP_accBalance",accChanges.p_accBalance);
+            */
         #endregion
     }
 }
--- BankingAPPConsole_Erick_Villegas/Program.cs
+++ BankingAPPConsole_Erick_Villegas/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace BankingAPPConsole_Erick_Villegas
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {   Accounts accChangesMade = new Accounts();
+            Console.WriteLine("Welcome to your Banking App.");
+            Console.WriteLine("Please select from one of the options below to begin.");
             #region Menu 
             Console.WriteLine("1. Create New Account");
             Console.WriteLine("2. Check Balance");
@@ -16,13 +18,82 @@ namespace BankingAPPConsole_Erick_Villegas
             Accounts account1 = new Accounts();
             #endregion
             bool continueBanking = true;
-
+            int balance = 0;
             while (continueBanking) {
                 int selection = Convert.ToInt32(Console.ReadLine());
                 switch(selection) {
                     case 1:
-                        //Console.WriteLine("Current Balance " + AccountBalance);
+                        Console.WriteLine("Please enter you full name.");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Please select from the following types of accounts:");
+                        Console.WriteLine("1.|Checkings| 2.|Savings| ");
+                        int typeselection = 0;
+                        string type = null;
+                        while (type == null){
+                            typeselection = Convert.ToInt32(Console.ReadLine());
+                            if (typeselection == 1){
+                            type = "Checkings";
+                            } else if (typeselection == 2){
+                            type = "Savings";
+                            } else{
+                            Console.WriteLine("Please select from one of the available options to continue.");
+                            }
+                        }
+                        Console.WriteLine("Your current balance will be 0 until your first deposit.");
+                        Console.WriteLine("Your account will be set to 'Active' unless you choose to set it to 'Inactive'.");
+                        bool isActive = true;
+                        Console.WriteLine("Please enter your email below to finish creating your "+ type +" account.");
+                        string email = Console.ReadLine();
+
+                        Accounts accChanges = new Accounts();
+                        accChanges.p_accName = name;
+                        accChanges.p_accType = type;
+                        accChanges.p_accBalance = balance;
+                        accChanges.p_isAccountActive = isActive;
+                        accChanges.p_accEmail = email;
+                        Console.WriteLine(accChangesMade.AccountChanges(accChanges));
                         break;
+                    case 2:
+                        Console.WriteLine("Please specify the account type for which you would like to see the balance.");
+                        bool checkBalance = true;
+                        string balanceSelection = null;
+                        Console.WriteLine("|Checkings| |Savings|");
+                        while (checkBalance)
+                        balanceSelection = Console.ReadLine();
+                        if (balanceSelection == "Checkings") {
+                            Accounts Check = accChangesMade.CheckingsBalance(balanceSelection);
+                            Console.WriteLine("Checkings Account Balance: " + Check.p_accBalance);
+                        }else if (balanceSelection == "Savings") {
+                            Accounts Check = accChangesMade.CheckingsBalance(balanceSelection);
+                            Console.WriteLine("Savings Account Balance " + Check.p_accBalance);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Please select the account you would like to withdraw from.");
+                        Console.WriteLine("|Checkings| |Savings|");
+                        string accountSelection = null;
+                        accountSelection = Console.ReadLine();
+                        if (accountSelection == "Checkings") {
+                            Console.WriteLine("Please enter the amount you want to withdraw.");
+                            int withdrawal_amount = Convert.ToInt32(Console.ReadLine());
+                            int temp_balance = accChangesMade.p_accBalance;
+                            temp_balance = temp_balance - withdrawal_amount;
+                            Console.WriteLine(withdrawal_amount + " has been withdrawn from your Checkings account");
+                            Console.WriteLine("Your current balance is " + temp_balance);
+                        }
+                        else if (accountSelection == "Savings") {
+                            Console.WriteLine("Please enter the amount you want to withdraw.");
+                            int withdrawal_amount = Convert.ToInt32(Console.ReadLine());
+                            int temp_balance = accChangesMade.p_accBalance;
+                            temp_balance = temp_balance - withdrawal_amount;
+                            Console.WriteLine(withdrawal_amount + " has been withdrawn from your Savings account");
+                            Console.WriteLine("Your current balance is " + temp_balance);
+
+                            //Accounts updateCheck = accChangesMade.Withdraw();
+                            //Console.WriteLine("Checkings Account Balance: "+ CheckWithdraw.p_accBalance);
+                        }
+                        break;
+
                 }
             }
             
